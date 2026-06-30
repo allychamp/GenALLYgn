758aaa8141fe8a268e340e32994527ef97587548
allychamp <ally.champoux@usherbrooke.ca>
2026-06-30 16:33:07 -0400

    change project directory

--- project/src/main.py
+++ project/src/main.py
@@ -1,7 +1,7 @@
 import pandas as pd
-from src.blast_links import load_blast
-from src.parser import parse_genbank
-from src.plotter import plot_genomes
+from DATA.PHAGE_genome_analysis.Phage_alignement_visualisation.project.src.blast_links import load_blast
+from DATA.PHAGE_genome_analysis.Phage_alignement_visualisation.project.src.parser import parse_genbank
+from DATA.PHAGE_genome_analysis.Phage_alignement_visualisation.project.src.plotter import plot_genomes
 from collections import defaultdict
 import textwrap
 import glob
